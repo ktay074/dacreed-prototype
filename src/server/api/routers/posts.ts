9ec2f10e7936b5658933d2613b961b3f94c01e3a
Cyra Moss <114317802+CyraMoss@users.noreply.github.com>
2023-04-25 20:43:38 +1200

    added profile user profile pic

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,10 +1,21 @@
 import { z } from "zod";
+import { clerkClient } from "@clerk/nextjs/server";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-    
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+        take: 100,
+    });
+
+    const users = await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+    })
+    console.log(users)
+
+    return posts
   }),
 });
