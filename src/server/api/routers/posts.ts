e674b53a6ff94d880438b1db21c90940123239fc
Cyra Moss <114317802+CyraMoss@users.noreply.github.com>
2023-04-26 01:15:35 +1200

    fixed TRPCError

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 import type {User} from "@clerk/nextjs/dist/api"
 import { clerkClient } from "@clerk/nextjs/server";
 
+import { TRPCError } from "@trpc/server";
+
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
@@ -29,11 +31,13 @@ export const postsRouter = createTRPCRouter({
     return posts.map((post) => {
         const author = users.find((user) => user.id === post.authorId)
 
-        if (!author || !author.username) 
-        throw new TRPCError({
-            code: "INTERNAL_SERVER_ERROR", 
-            message: "Author not found"
-        });
+        if (!author) {
+            console.error("AUTHOR NOT FOUND", post);
+            throw new TRPCError({
+              code: "INTERNAL_SERVER_ERROR",
+              message: `Author for post not found. POST ID: ${post.id}, USER ID: ${post.authorId}`,
+            });
+          }
 
         return {
         post,
