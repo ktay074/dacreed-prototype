1efbd5d01ae55d0d8551d0caa9a64766eab848c1
Cyra Moss <114317802+CyraMoss@users.noreply.github.com>
2023-05-08 23:02:10 +1200

    changed course pref api

--- src/server/api/routers/coursepreferences.tsx
+++ src/server/api/routers/coursepreferences.tsx
@@ -1,18 +1,17 @@
-import {  z } from "zod";
+import { z } from "zod";
 import { 
     createTRPCRouter, 
     privateProcedure, 
 } from "~/server/api/trpc";
 
 export const coursePreferenceRouter = createTRPCRouter({
-
-
   create: privateProcedure
   .input(
       z.object({
         simplicityPref: z.array(z.number()),
         humourPref: z.array(z.number()),
         professionalismPref: z.array(z.number()),
+        id: z.optional(z.string()), // add the id property and set it to undefined
       })
   )
   .mutation(async ({ ctx, input }) => {
@@ -20,14 +19,11 @@ export const coursePreferenceRouter = createTRPCRouter({
       data: {
         simplicityPref: { set: input.simplicityPref },
         humourPref: { set: input.humourPref },
-        proffesionalismPref: { set: input.professionalismPref }
+        proffesionalismPref: { set: input.professionalismPref },
+        id: input.id ?? "", // include the id property in the data
       },
     });
 
     return coursePref;
   }),
-
-
-  
-
 });
