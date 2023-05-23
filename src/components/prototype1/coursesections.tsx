d99daed15f1fbb9b1f8e3819211a4deb6a98f61c
Kevin Tay <Kevint13@gmail.com>
2023-05-23 22:52:45 +1200

    added course sections component and loading gif - prisma schema also updated

---
+++ src/components/prototype1/coursesections.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import MoreOptionsIcon from "../../images/prototype1/moreoptionsicon.png";
+
+interface SectionsProps {
+    title: string; 
+    description: string; 
+}
+
+const Sections: React.FC<SectionsProps> = ({ title, description }) => {
+    return(
+        <div className="mr-80 mt-5 font-sans">
+            <div className="bg-[#393DE3] px-8 py-4 flex">
+                <h3 className="text-[#ECECEC] mt-2">{title}</h3>
+                <div className="ml-60 flex">
+                    <button className="bg-[#FFF8ED] text-black rounded-lg mr-7 px-6 py-2 border-2 border-black">
+                      <p>ADD CONTENT</p>  
+                    </button>
+                    <button className="bg-[#FFF8ED] rounded-lg p-2 border-2 border-black"><Image src={MoreOptionsIcon} alt="more options"></Image></button>
+                </div>
+            </div>
+            <div className="border-[#D0D0D0] border-2 bg-[#F6F2FF] px-4 py-4">
+                <p>{description}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Sections
--- src/pages/prototype1/courseeditor.tsx
+++ src/pages/prototype1/courseeditor.tsx
@@ -1,12 +1,27 @@
 import LeftNavBar from "~/components/prototype1/leftsidenav";
+import { api } from "~/utils/api"; 
+import Sections from "~/components/prototype1/coursesections";
 
-export default function CourseEditor() {
+
+const CoursesPage: React.FC = () => {
+    const retrieveCourses = api.courses.getAll.useQuery(); 
+    
     return (
-        <div>
-            <div>
+        <div className="flex">
+            <div className="flex-grow">
             <LeftNavBar></LeftNavBar>
+            </div>
+
+            <div className="mr-80">
+            {/* Course sections */}
+                <div>
+                    {/* {retrieveCourses.map((fullCourse) => fullCourse)} */}
+                    <Sections title={"Whistle Blowing"} description="The purpose of this policy is to set out the processes" />
+                </div>
             </div>
         </div>    
     )
     
   }
+
+export default CoursesPage
